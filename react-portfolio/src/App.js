48f271f337ef0099e829344d9e4ac8e102a3ae96
Sofya Vibe <sofyavibe@gmail.com>
2022-12-24 12:07:45 +0300

    fix

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
+    <div>
+      <Header />
+    </div>
     
+
   );
 }
 
