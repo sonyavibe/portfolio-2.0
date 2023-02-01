bb74dd48b2ac834ad02e320531bd0874ab162efe
Sofya Vibe <sofyavibe@gmail.com>
2023-02-01 21:44:12 +0700

    fix

--- react-portfolio/src/components/work/Data.jsx
+++ react-portfolio/src/components/work/Data.jsx
@@ -3,7 +3,7 @@ import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.PNG";
 import Work4 from "../../assets/work4.png";
-import Work5 from "../../assets/work5.jpg";
+import Work5 from "../../assets/work5.PNG";
 
 export const projectsData = [
   {
